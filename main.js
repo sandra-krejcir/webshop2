a0d5dab30dbe305b941ec51b617f0457dc44d9ab
Sandra Krejcir <krejcir.sandra@gmail.com>
2021-02-13 09:34:40 +0100

    last commit- everything works

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
-const urlParams = new URLSearchParams(window.location.search)
-const id = urlParams.get("id")
-const url = "https://kea-alt-del.dk/t7/api/products/" + id;
+const url = "https://kea-alt-del.dk/t7/api/products";
 fetch(url)
-.then((res) -> res.json())
-.then((data) -> listProducts(data))
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    listProducts(data);
+  });
 
-function listProducts(product) {
-    console.log(product);
-    const template = document.querySelector("#theList").content;
-    const copy = template.cloneNode(true);
-    copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
-    copy.querySelector("a").href = `product.html?id=${product.id}`;
-    copy.querySelector(".subtle").textContent = `${product.articletype} | ${product.brandname}`;
-    copy.querySelector("h3").textContent = product.productdisplayname;
-    copy.querySelector(".price").textContent = product.price;
-    const parent = document.querySelector("main");
-    parent.appendChild(copy);
+function listProducts(data) {
+  data.forEach(showProducts);
+}
+
+function showProducts(product) {
+  console.log(product);
+  const template = document.querySelector("#theList").content;
+  const copy = template.cloneNode(true);
+  copy.querySelector(
+    "img"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  copy.querySelector(
+    ".subtle"
+  ).textContent = `${product.articletype} | ${product.brandname}`;
+  copy.querySelector("h3").textContent = product.productdisplayname;
+  copy.querySelector(".price").textContent = `Price: ${product.price},00 DKK`;
+  if (product.soldout) {
+    copy.querySelector("article").classList.add("soldOut");
+  }
+  if (product.discount) {
+    copy.querySelector(
+      ".price"
+    ).textContent = `Price: ${product.price},00 DKK / -${product.discount}%`;
+  }
+  copy.querySelector("a").href = `product.html?id=${product.id}`;
+  const parent = document.querySelector("main");
+  parent.appendChild(copy);
 }
---
+++ product.js
@@ -0,0 +1,29 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+console.log(id);
+
+const url = "https://kea-alt-del.dk/t7/api/products/" + id;
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => viewProduct(data));
+
+function viewProduct(product) {
+  console.log(product);
+  document.querySelector(".breadcrumbs .brandName").textContent =
+    product.brandname;
+  document.querySelector(".breadcrumbs .prodName").textContent =
+    product.productdisplayname;
+  document.querySelector(
+    "img"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  document.querySelector("img").alt = product.productdisplayname;
+  document.querySelector("h3").textContent = product.productdisplayname;
+  document.querySelector(
+    ".sticker"
+  ).textContent = `${product.articletype} | ${product.brandname}`;
+  document.querySelector(".prod").textContent = product.productdisplayname;
+  document.querySelector(
+    ".season"
+  ).textContent = `${product.season}, ${product.productionyear}`;
+  document.querySelector(".ID").textContent = product.id;
+}
